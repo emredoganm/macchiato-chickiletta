bdeab5c95ce5ed0b6834cf664e4e4e125c3d8b31
Emre Dogan <emre.dogan@trendyol.com>
2021-06-21 03:46:45 +0300

    Refactor category filters after app context provider creation

--- src/components/category-filters.tsx
+++ src/components/category-filters.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { useAppContext } from "../context";
 import "../styles/category-filters.css";
 
 export const CategoryFilters: React.FC = () => {
+  const { category, categories, setCategory } = useAppContext();
+  const handleClickCategory = (name?: string) => () => setCategory(name);
+
   return (
     <ul data-testid="category-filters" className="category-filters">
-      <li className="selected">All Coffees</li>
-      <li>Hot</li>
-      <li>Iced</li>
+      <li className={!category ? "selected" : ""} data-testid="all-categories" onClick={handleClickCategory()}>
+        All Coffees
+      </li>
+
+      {categories.map((categoryItem) => (
+        <li
+          className={category === categoryItem ? "selected" : ""}
+          onClick={handleClickCategory(categoryItem)}
+          key={`categoryFilter@${categoryItem}`}
+          data-testid={`category-filter-${categoryItem}`}
+        >
+          {categoryItem}
+        </li>
+      ))}
     </ul>
   );
 };
--- src/test/components/category-filters.spec.tsx
+++ src/test/components/category-filters.spec.tsx
@@ -1,10 +1,50 @@
-import { render } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+import { datatype } from "faker";
 import { CategoryFilters } from "../../components";
+import { renderWithContext } from "../helpers";
 
 describe("Category filters tests", () => {
-  it("should render as expected", () => {
-    const { container } = render(<CategoryFilters />);
+  it("should select all categories when no category selected", () => {
+    const { getByTestId, baseElement } = renderWithContext(<CategoryFilters />, { category: undefined });
 
-    expect(container).toBeInTheDocument();
+    expect(baseElement.querySelectorAll("li.selected")).toHaveLength(1);
+    expect(getByTestId("all-categories").className).toStrictEqual("selected");
+  });
+
+  it("should set category undefined when all categories clicked", () => {
+    const setCategory = jest.fn();
+    const { getByTestId, baseElement } = renderWithContext(<CategoryFilters />, { setCategory });
+
+    expect(baseElement.querySelectorAll("li.selected")).toHaveLength(1);
+    expect(getByTestId("all-categories").className).not.toStrictEqual("selected");
+
+    fireEvent.click(getByTestId("all-categories"));
+
+    expect(setCategory).toHaveBeenCalledWith(undefined);
+  });
+
+  it("should be selected when category is defined", () => {
+    const categories = [...new Array(datatype.number({ min: 1, max: 10 }))].map(datatype.uuid);
+    const category = categories[0];
+    const { getByTestId, baseElement } = renderWithContext(<CategoryFilters />, { category, categories });
+
+    expect(baseElement.querySelectorAll("li.selected")).toHaveLength(1);
+    expect(getByTestId("all-categories").className).not.toStrictEqual("selected");
+    expect(getByTestId(`category-filter-${category}`).className).toStrictEqual("selected");
+  });
+
+  it("should call setCategory function with desired category", () => {
+    const setCategory = jest.fn();
+    const categories = [...new Array(datatype.number({ min: 4, max: 10 }))].map(datatype.uuid);
+    const category = categories[0];
+    const { getByTestId, baseElement } = renderWithContext(<CategoryFilters />, { category, categories, setCategory });
+
+    expect(baseElement.querySelectorAll("li.selected")).toHaveLength(1);
+    expect(getByTestId("all-categories").className).not.toStrictEqual("selected");
+    expect(getByTestId(`category-filter-${category}`).className).toStrictEqual("selected");
+
+    fireEvent.click(getByTestId(`category-filter-${categories[2]}`));
+
+    expect(setCategory).toHaveBeenCalledWith(categories[2]);
   });
 });
